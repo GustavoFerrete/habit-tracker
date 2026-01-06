761cba580c08a21c04baf6b8921c1842da0126cc
Gustavo Ferrete <gustavoferrete2000@outlook.com>
2026-01-06 15:08:51 -0300

    Gustavo - Adicionando funcionalidade de login e de usuario, retornando a tabela sessions tambem

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SiteController;
 
-
+// Site
 Route::get('/', [SiteController::class, 'index']);
+
+// Login
+Route::get('/login', [LoginController::class, 'index']);
