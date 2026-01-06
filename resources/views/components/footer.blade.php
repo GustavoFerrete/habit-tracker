433980888d7d706fccbbecde416575bd49b51c9a
Gustavo Ferrete <gustavoferrete2000@outlook.com>
2026-01-06 12:28:23 -0300

    Gustavo - Init components header/footer/layout

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-white border-t-2 p-4">
+    <p>
+        Criado por Gustavo Ferrete. O código fonte está no <a href="#" class="underline">GitHub.</a>
+    </p>
+</footer>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,11 @@
+<header class="bg-white border-b-2 flex items-center justify-between p-4">
+    {{-- LOGO --}}
+    <div>
+        logo aqui
+    </div>
+
+    {{-- GitHub --}}
+    <div>
+        github aqui
+    </div>
+</header>
