1e219d74e909c05b1e94f127bad2e26987db0d34
Gustavo Ferrete <gustavoferrete2000@outlook.com>
2026-01-06 11:56:34 -0300

    Setup tailwind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,19 @@
-<h1>
-    Welcome to the home page
-</h1>
-
-<p>
-    Olá, {{ $name }}
-</p>
-
-<p>
-    Seus hábitos são:
-</p>
-
-<ul>
-    @foreach ($habits as $habit)
-        <li>{{ $habit }}</li>
-    @endforeach
-</ul>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        {{ config('app.name') }}
+    </title>
+    
+    @vite('resources/css/app.css')
+</head>
+<body>
+    
+    <h1 class="text-3x1 font-bold underline">
+        Hello world!
+    </h1>
+    
+</body>
+</html>
